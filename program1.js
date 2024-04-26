f2b5f399f6009add9fe40a649625113e2244ecf4
shubham kumar <76179134+shubham0540@users.noreply.github.com>
2024-04-26 13:25:02 +0530

    Add files via upload
    test

--- program1.js
+++ program1.js
@@ -1,9 +1,25 @@
 function smallestMissingPositiveInteger(nums) {
-
-
-    // Implement the function smallest_missing_positive_integer
-  
+  let n = nums.length;
+  for (let i = 0; i < n; i++) {
+      if (nums[i] <= 0 || nums[i] > n) {
+          nums[i] = 0;
+      }
   }
+  for (let i = 0; i < n; i++) {
+      let num = Math.abs(nums[i]);
+      if (num !== 0) {
+          nums[num - 1] = -Math.abs(nums[num - 1]);
+      }
+  }
+  for (let i = 0; i < n; i++) {
+      if (nums[i] >= 0) {
+          return i + 1;
+      }
+  }
+
+  return n + 1;
+
+}
   
-  module.exports = smallestMissingPositiveInteger;
+module.exports = smallestMissingPositiveInteger;
 
--- program2.js
+++ program2.js
@@ -1,7 +1,20 @@
 function longestSubstring(s) {
-
-
-    // Implementation of longestSubstring function
-}
+        let longest = 0;
+        let start = 0;
+        const map = new Map();
+    
+        for (let i = 0; i < s.length; i++) {
+            const char = s[i];
+    
+            if (map.has(char) && map.get(char) >= start) {
+                start = map.get(char) + 1;
+            }
+    
+            map.set(char, i);
+            longest = Math.max(longest, i - start + 1);
+        }
+    
+        return longest;
+    }
 
 module.exports = { longestSubstring };
